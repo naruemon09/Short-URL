c4213856c137c0b19b4759132dc9877d6ec0aa60
NaruemonKhongao <156495906+NaruemonKhongao@users.noreply.github.com>
2025-05-31 17:33:27 +0700

    commit

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
