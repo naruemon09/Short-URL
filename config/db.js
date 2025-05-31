4691bf414ca356a006696493a8404c6fa3e4ffbf
NaruemonKhongao <156495906+NaruemonKhongao@users.noreply.github.com>
2025-06-01 01:59:18 +0700

    commit

--- config/db.js
+++ config/db.js
@@ -5,12 +5,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const connection = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: false 
+  connectionString: process.env.DATABASE_URL 
 });
 
 console.log('Connected to MySQL database');
