915f1e1605efa723ed5b6d2734fbe6df6386eac9
NaruemonKhongao <156495906+NaruemonKhongao@users.noreply.github.com>
2025-05-31 22:29:42 +0700

    commit

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const connection = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: false,
+    ssl: false
   });
 
 
