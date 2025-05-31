27311f21c7378116f6468d1c37a1eac28518cb1d
NaruemonKhongao <156495906+NaruemonKhongao@users.noreply.github.com>
2025-06-01 00:57:12 +0700

    commit

--- config/db.js
+++ config/db.js
@@ -1,30 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import pkg from 'pg';
+import mysql from 'mysql2/promise';
 
-const { Pool } = pkg;
-
-const connection = new Pool({
+const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: false
   });
 
-
-  async function testConnection() {
-  try {
-    const res = await connection.query('SELECT NOW()')
-    console.log('Connected to PostgreSQL at:', res.rows[0].now)
-  } catch (error) {
-    console.error('Error connecting to database:', error)
-  }
-}
-
-testConnection()
-
-console.log('Connected to database');
+console.log('Connected to MySQL database');
 
 export default connection;
