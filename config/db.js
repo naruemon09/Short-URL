1d217177c1a064392dfc5d882cda5e9233ec6f45
NaruemonKhongao <156495906+NaruemonKhongao@users.noreply.github.com>
2025-06-06 12:56:25 +0700

    commit

--- config/db.js
+++ config/db.js
@@ -5,10 +5,12 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const connection = new Pool({
-  connectionString: process.env.DATABASE_URL ,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: false
 });
 
 
