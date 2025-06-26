c86ffed6e4f1e125b2cbc7ed3962edd8801becea
NaruemonKhongao <156495906+NaruemonKhongao@users.noreply.github.com>
2025-06-27 00:47:00 +0700

    commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import QRCode from 'qrcode';
 
 app.set('view engine','ejs');
 app.use(express.json());
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
